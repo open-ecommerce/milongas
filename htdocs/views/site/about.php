6b7d32bb292837746ee0d0f2a8728a538fa21ca9
Eduardo Silva <eduardo@open-ecommerce.org>
2016-08-16 09:56:35 +0100

    about

--- htdocs/views/site/about.php
+++ htdocs/views/site/about.php
@@ -1,8 +1,7 @@
 <?php
-use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title                   = 'About this system';
+$this->title = 'About this system';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
@@ -14,16 +13,21 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="container">
             <div class="row">
                 <div class="col-md-12 text-center">
-                    <p class="lead">This is the internal managment system for the Destitute Asylum Seekers Drop-In.<br>
-                    If you need to work with the system please signup or ask for your credentials.</p>
-                    <br>
-                    <hr>
-                    <br>
-                    <p>This system was developed using Yii2 tuned up with phundament.</p>
-                    <p>Drop-in manager is a free open-source application under the terms of BSD Licence.</p>
-                    <p>You are more than welcome to collaborate to improve the system and/or to reuse the code in the way you want.</p>
-                    <br>
-                    <br>
+                    <div class="row">
+                      <div class="large-6 columns">
+                        <h3> How can we help you?</h3>
+                        <p>If you are thinking of adding text to your helpline, please contact us.</p>
+                        <p> We will help you with our expertise and build a system to cater for your needs.</p>
+                        <p>Send us an email to <a href="mailto:info@helptext.uk?Subject=Landing Page" target="_top">info@helptext.uk</a> or text us to 07879387106, and we will get back to you within 24 hours.</p>
+                      <br>
+                        <a target="_blank" href="http://www.open-ecommerce.org">
+                            <img src="/assets/img/logos/powered-by-open-ecommerce-org.png" alt="Open-ecommerce.org" class="oe">
+                        </a>
+                      </div>
+                      <div class="large-6 columns">
+                        <img src="/assets/img/images/helptext-offices_4.jpg">
+                      </div>
+                    </div>
                     <p><a style="line-height: 1.3em;" title="github of open-ecommerce.org" href="https://github.com/open-ecommerce" target="_blank">
                     <img style="display: block; margin-left: auto; margin-right: auto;" alt="open ecommerce at github social coding" src= <?php echo '/media/open-ecommerce-at-github-social-coding.png'; ?> height="86" width="239"></a>&nbsp;</p>
                 </div>
